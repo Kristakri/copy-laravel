8d1125904fcf88aff4f8daa7b588c984c2e729f8
Kristakri <nago.gun@mail.ru>
2019-10-06 17:14:19 +0300

    frontend

---
+++ resources/views/Head/Index.blade.php
@@ -0,0 +1,62 @@
+<script src="{{asset('js/search/search.js')}}" type="application/javascript"></script>
+<div class="head">
+    <div class="row">
+        <div class="head-left-block col-md-8 ml-3">
+            <div class="mt-3">
+                <a href="">
+                    <img src="{{ asset('storage/image/logo/logo.png') }}" alt="logo" class="logo">
+                </a>
+            </div>
+            <div class="title mt-3">
+                <p>
+                    Книжный портал <br>
+                    с персональными рекомендациями <br>
+                    и личными коллекциями
+                </p>
+            </div>
+            <div class="stats mt-3">
+                <div class="stat">
+                    <p>
+                        <b>15 000 000</b> <br>
+                        оценок книг
+                    </p>
+                </div>
+                <div class="stat">
+                    <p>
+                        <b>100 000</b> <br>
+                        рецензий на книги
+                    </p>
+                </div>
+                <div class="stat">
+                    <p>
+                        <b>50 000</b> <br>
+                        книг в коллекции
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="head-right-block col-md-4 row justify-content-end">
+            <div class="register-form">
+                <form action="#">
+                    <p class="register-form-title">Зарегистрируйтесь или <a href="#">войдите</a></p>
+                    <div class="form-email mt-2">
+                        <input type="email" class="form-email-input" placeholder="Email...">
+                    </div>
+                    <p class="register-form-password-description mt-3">Пароль будет создан автоматически и отправлен <br> вам на почту,
+                        <a href="#">или ввести пароль самостоятельно</a>
+                    </p>
+                    <button class="register-button mt-3">Зарегистрироваться</button>
+                    
+                    <div class="social-icons mt-3">
+                        <p class="mb-2">Регистрация через соц. сеть</p>
+                        <a href="#" class="social-link"><img src="https://s.livelib.ru//img/skins/ll2015b/soc/vkontakte.svg?v=2" alt="vk" class="social-img"></a>
+                        <a href="#" class="social-link"><img src="https://s.livelib.ru//img/skins/ll2015b/soc/facebook.svg?v=2" alt="facabook" class="social-img"></a>
+                        <a href="#" class="social-link"><img src="https://s.livelib.ru//img/skins/ll2015b/soc/google.svg?v=2" alt="google" class="social-img"></a>
+                        <a href="#" class="social-link"><img src="https://s.livelib.ru//img/skins/ll2015b/soc/mailru.svg?v=2" alt="mail" class="social-img"></a>
+                        <a href="#" class="social-link"><img src="https://s.livelib.ru//img/skins/ll2015b/soc/twitter.svg?v=2" alt="twitter" class="social-img"></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
